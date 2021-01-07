b7ebc0cec1bcd8d365cbf2ffa6e77ceb34a5f0cd
lizi196 <71314682+lizi196@users.noreply.github.com>
2021-01-07 07:39:44 +0000

    lastwork

--- src/work9/work12_7.java
+++ src/work9/work12_7.java
@@ -2,5 +2,33 @@ package work9;
 import java.util.*;
 
 public class work12_7 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter a binary number: ");
+		String B = input.nextLine();
+		double result = 0;
+		int f = 0;
+		for(int i = B.length() - 1; i >= 0; i--) {
+			char BChar = B.charAt(i);
+			double x = 0;
+			try {
+				if(BChar == '0')
+					continue;
+				else if(BChar == '1') {
+					x = Math.pow(2, B.length() - 1 - i);
+				}
+				else throw new Exception("自定义的异常");
+			}
+			catch(Exception e) {
+				if(f == 0)
+					System.out.println("输入的字符串不是二进制字符串!");
+				f = 1;
+			}
+			result = result + x;
+		}
+		if(f == 0)
+			System.out.println("The decimal value for Binary number "
+					+ B + " is " + (int)result);
+		}
 
 }
