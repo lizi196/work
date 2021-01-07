691ec35a0d909444b63eb72d3afba7e1fe944129
lizi196 <71314682+lizi196@users.noreply.github.com>
2021-01-07 07:18:13 +0000

    Update work9_4.java

--- src/work5/work9_4.java
+++ src/work5/work9_4.java
@@ -1,5 +1,14 @@
 package work5;
 
+import java.util.Random;
+
 public class work9_4 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random random1 = new Random(1000);
+		for(int i = 1; i <= 50; i++) {
+			System.out.println(random1.nextInt(100));
+		}
+	}
 
 }
