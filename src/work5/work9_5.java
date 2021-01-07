7ffc27a4cce694d64acbf320d309ebc1d45da9fe
lizi196 <71314682+lizi196@users.noreply.github.com>
2021-01-07 07:18:47 +0000

    lastwork

--- src/work5/work9_5.java
+++ src/work5/work9_5.java
@@ -1,5 +1,21 @@
 package work5;
 
+import java.util.GregorianCalendar;
+
 public class work9_5 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GregorianCalendar gregorianCalendar = new GregorianCalendar();
+		int m =  gregorianCalendar.get(GregorianCalendar.MONTH);
+		m += 1;
+		System.out.println(gregorianCalendar.get(GregorianCalendar.YEAR) + "å¹?" + m + 
+				"æœ?" + gregorianCalendar.get(GregorianCalendar.DAY_OF_MONTH) + "æ—?");
+		gregorianCalendar.setTimeInMillis(1234567898765L);
+		m =  gregorianCalendar.get(GregorianCalendar.MONTH);
+		m += 1;
+		System.out.println(gregorianCalendar.get(GregorianCalendar.YEAR) + "å¹?" + m + 
+				"æœ?" + gregorianCalendar.get(GregorianCalendar.DAY_OF_MONTH) + "æ—?");
+	}
 
 }
+
